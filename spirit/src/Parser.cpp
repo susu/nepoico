6a2bb0b93837f2f2d74126fa0da4197888f30838
Marton Suranyi <marton.suranyi@gmail.com>
2012-05-23 15:50:32 +0200

    parser with tests

--- spirit/src/Parser.cpp
+++ spirit/src/Parser.cpp
@@ -10,5 +10,24 @@ Parser::~Parser()
 {
 }
 
+bool Parser::parseAndAdd( std::string const & bytes )
+{
+  using namespace boost::spirit::classic;
+  char type;
+  int length;
+  std::string value;
 
+  bool ret = parse( bytes.c_str(),
+    (anychar_p)[ assign_a(type) ] >>
+    (anychar_p)[ assign_a(length) ] >>
+    (repeat_p (boost::ref(length))[ anychar_p ])[ assign_a(value) ]
+    ,space_p).full;
+
+  // hacky way
+  // TODO test on different platform/compiler than Linux/GCC
+  int t = (int(type) & 0xff);
+
+  (*this)[ t ] = value;
+  return ret;
+}
 
--- spirit/test/TestParser.hpp
+++ spirit/test/TestParser.hpp
@@ -1,5 +1,6 @@
 #include "cxxtest_ext.hpp"
 
+#include <Parser.hpp>
 
 class TestParser : public CxxTest::TestSuite
 {
@@ -7,7 +8,65 @@ class TestParser : public CxxTest::TestSuite
     void test_ctor()
     {
       TCH;
+      Parser p;
     }
 
+    void test_parseAndAdd_should_store_data_in_map_in_case_of_proper_input()
+    {
+      TCH;
+      // Arrange
+      std::string bytes( "00" );
+      bytes[0] = char(255);
+      bytes[1] = 3;
+      bytes.append("bab");
+
+      Parser p;
+
+      // Act
+      bool result = p.parseAndAdd( bytes );
+
+      // Assert
+      TS_ASSERT_EQUALS( true, result );
+      TS_ASSERT_EQUALS( "bab", p[255] );
+    }
+
+    void test_parseAndAdd_should_store_data_in_map_in_case_of_proper_input_unsigned_char_issue()
+    {
+      TCH;
+      // Arrange
+      std::string bytes( "00" );
+      bytes[0] = char(55);
+      bytes[1] = 5;
+      bytes.append("hello");
+
+      Parser p;
+
+      // Act
+      bool result = p.parseAndAdd( bytes );
+
+      // Assert
+      TS_ASSERT_EQUALS( true, result );
+      TS_ASSERT_EQUALS( "hello", p[55] );
+    }
+
+    void xtest_parseAndAdd_should_not_store_data_in_map_in_case_of_wrong_input()
+    {
+      TCH;
+      // Arrange
+      std::string bytes( "00" );
+      bytes[0] = char(255);
+      bytes[1] = 3;
+      bytes.append("babaaa");
+
+      Parser p;
+
+      // Act
+      bool result = p.parseAndAdd( bytes );
+
+      // Assert
+      TS_ASSERT_EQUALS( false, result );
+      TS_ASSERT_DIFFERS( "bab", p[255] );
+      TS_ASSERT_DIFFERS( "babaaa", p[255] );
+    }
 };
 
