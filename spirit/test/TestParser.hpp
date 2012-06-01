c710ebcde111b906e8b492129bbfbf511c895ba5
Marton Suranyi <marton.suranyi@gmail.com>
2012-06-01 15:32:11 +0200

    tests updated, multiple TLV in one string added.

--- spirit/test/TestParser.hpp
+++ spirit/test/TestParser.hpp
@@ -1,14 +1,14 @@
 #include "cxxtest_ext.hpp"
 
-#include <Parser.hpp>
+#include <ClassicParser.hpp>
 
-class TestParser : public CxxTest::TestSuite
+class TestClassicParser : public CxxTest::TestSuite
 {
   public:
     void test_ctor()
     {
       TCH;
-      Parser p;
+      ClassicParser p;
     }
 
     void test_parseAndAdd_should_store_data_in_map_in_case_of_proper_input()
@@ -16,7 +16,7 @@ class TestParser : public CxxTest::TestSuite
       TCH;
       // Arrange
       std::string bytes( createTlv( char(255), 3, "bab" ) );
-      Parser p;
+      ClassicParser p;
 
       // Act
       bool result = p.parseAndAdd( bytes );
@@ -31,7 +31,7 @@ class TestParser : public CxxTest::TestSuite
       TCH;
       // Arrange
       std::string bytes( createTlv(char(55), 5, "hello") );
-      Parser p;
+      ClassicParser p;
 
       // Act
       bool result = p.parseAndAdd( bytes );
@@ -46,7 +46,7 @@ class TestParser : public CxxTest::TestSuite
       TCH;
       // Arrange
       std::string bytes( createTlv( char(255), 3, "babaaa") );
-      Parser p;
+      ClassicParser p;
 
       // Act
       bool result = p.parseAndAdd( bytes );
@@ -65,7 +65,7 @@ class TestParser : public CxxTest::TestSuite
       tlvs.push_back( createTlv( char(150), 2,    "ha" ) );
       tlvs.push_back( createTlv( char(20),  5, "hahah" ) );
       tlvs.push_back( createTlv( char(223), 1,     "x" ) );
-      Parser p;
+      ClassicParser p;
 
       // Act
       bool result = p.parseAndAdd( tlvs[0] );
@@ -77,6 +77,28 @@ class TestParser : public CxxTest::TestSuite
       TS_ASSERT_EQUALS( "ha",    p[150] );
       TS_ASSERT_EQUALS( "hahah", p[20] );
       TS_ASSERT_EQUALS( "x",     p[223] );
+    }
+
+    void test_parseAndAdd_should_process_multiple_TLV_in_one_string()
+    {
+      TCH;
+      // Arrange
+      std::string sentence = "This is a sentence.";
+      std::string bytes = createTlv( char(45), 3, "xyz" );
+      bytes            += createTlv( char(140), 5, "hello" );
+      bytes            += createTlv( char(22), 1, "h" );
+      bytes            += createTlv( char(244), sentence.length(), sentence );
+
+      ClassicParser p;
+      // Act
+      bool results = p.parseAndAdd(bytes);
+
+      // Assert
+      TS_ASSERT_EQUALS( true, results );
+      TS_ASSERT_EQUALS( sentence, p[244] );
+      TS_ASSERT_EQUALS( "xyz", p[45] );
+      TS_ASSERT_EQUALS( "hello", p[140] );
+      TS_ASSERT_EQUALS( "h", p[22] );
     }
 
   protected:
